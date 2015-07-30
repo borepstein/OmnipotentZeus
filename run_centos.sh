c21e5d5bdc9d6673f57bcebd8f2a9b62704a30d0
Kenny Mu Li <kenny@kennyl.com>
2015-07-29 22:01:38 -0700

    add centos compat

---
+++ run_centos.sh
@@ -0,0 +1,5 @@
+yum install python-pip mysql-server libmysqlclient-dev python-dev python-lxml -y
+pip install mysql-python
+pip install sqlalchemy
+pip install beautifulsoup4
+screen python OmnipotentZeus/hermes.py
