5aca8503fbe0adc6bf458f2ce1a332235de10290
sibi.m <sibim024@gmail.com>
2015-08-26 16:30:47 +0530

    Removed unwanted files for windows version

--- run_azure.sh
+++
@@ -1,4 +0,0 @@
-apt-get install python-pip mysql-server libmysqlclient-dev python-dev --yes
-pip install mysql-python
-pip install sqlalchemy
-screen python OmnipotentZeus/hermes_azure.py
--- run_centos.sh
+++
@@ -1,6 +0,0 @@
-yum install epel-release -y
-yum install screen python-pip mysql-server mysql-devel.x86_64 python-devel python-lxml libaio* gcc wget make libibverbs.x86_64 -y
-pip install mysql-python
-pip install sqlalchemy
-pip install beautifulsoup4
-screen python OmnipotentZeus/hermes.py
