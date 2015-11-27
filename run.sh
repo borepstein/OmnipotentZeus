00f96176ce8f6c5fb185e57246fac8a52ccfef70
Sibi M <sibim024@gmail.com>
2015-11-27 16:09:03 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 apt-get update
 apt-get install python-pip mysql-server libmysqlclient-dev python-dev python-lxml --yes
-pip install -y mysql-python
-pip install -y sqlalchemy
-pip install -y beautifulsoup4
+pip install mysql-python
+pip install sqlalchemy
+pip install beautifulsoup4
 screen python OmnipotentZeus/hermes.py
