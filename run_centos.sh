d4ea9d096b5b998ca5f9d25e08ae802089bfc2f5
Sibi M <sibim024@gmail.com>
2016-09-16 16:50:53 +0530

    Update run_centos.sh

--- run_centos.sh
+++ run_centos.sh
@@ -1,5 +1,11 @@
+yum groupinstall 'Development Tools' -y
 yum install epel-release -y
-yum install screen python-pip mysql-server mysql-devel.x86_64 python-devel python-lxml libaio* gcc wget make libibverbs.x86_64 -y
+wget http://dl.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-8.noarch.rpm
+rpm -ivh epel-release-7-8.noarch.rpm
+#rpm -ivh epel-release-6-8.noarch.rpm
+yum install mysql-community-server -y
+yum install mysql-devel
+yum install git vim screen python-pip mysql-server mysql-devel.x86_64 python-devel python-lxml libaio* gcc wget make libibverbs.x86_64 -y
 pip install mysql-python
 pip install sqlalchemy
 pip install beautifulsoup4
