861187f5417f6f7f9ef2900705890faf618857d6
sibi <sibi.m@inapp.com>
2016-09-19 19:04:46 +0530

    renamed aggregate script name

--- aggregate_gen.py
+++ aggregate_gen.py
@@ -34,56 +34,104 @@ class AggregateGenerator:
             if self.processordata == "y":
                 table = "xiaoice_processordata"
                 field = "performance"
-                self.cur.execute("SELECT vm_id, cores_id, os_id FROM %s WHERE vm_id = %s" % (table, result['id']))
-                res = self.cur.fetchone()
 
-                agg_id = self.cur.execute("INSERT INTO xiaoice_processoraggdata (vm_id, cores_id, os_id) VALUES (%s, %s, %s)" % (res['vm_id'], res['cores_id'], res['os_id']))
-                self.con.commit()
+                try:
+                    self.cur.execute("SELECT vm_id, cores_id, os_id FROM %s WHERE vm_id = %s" % (table, result['id']))
+                    res = self.cur.fetchone()
 
-                for term in self.term_list:
-                    output = self.agg_data(term, table, field)
-                    self.cur.execute("UPDATE xiaoice_processoraggdata SET %s_min = %s, %s_25 = %s, %s_75 = %s, %s_max = %s, %s_median = %s) WHERE id = %s" % (term, output['resultmin'], term, output['result25'], term, output['result75'], term, output['resultmax'], term, output['resultmedian'], agg_id))
+                    self.cur.execute("INSERT INTO xiaoice_processoraggdata (vm_id, cores_id, os_id) VALUES (%s, %s, %s)" % (res['vm_id'], res['cores_id'], res['os_id']))
+                    self.con.commit()
+                    agg_id = self.cur.lastrowid
+
+                    for term in self.term_list:
+                        output = self.agg_data(term, table, field, res['vm_id'])
+                        if output:
+                            self.cur.execute("UPDATE xiaoice_processoraggdata SET %s_min = %s, %s_25 = %s, %s_75 = %s, %s_max = %s, %s_median = %s WHERE id = %s" % (term, output['resultmin'], term, output['result25'], term, output['result75'], term, output['resultmax'], term, output['resultmedian'], agg_id))
+                            self.con.commit()
+                except Exception as e:
+                    pass
 
             if self.memorydata == "y":
                 table = "xiaoice_memorydata"
                 field = "bandwidth"
 
-                for term in self.term_list:
-                    output = self.agg_data(term, table, field)
+                try:
+                    self.cur.execute("SELECT vm_id, cores_id, os_id FROM %s WHERE vm_id = %s" % (table, result['id']))
+                    res = self.cur.fetchone()
+
+                    self.cur.execute("INSERT INTO xiaoice_memoryaggdata (vm_id, cores_id, os_id) VALUES (%s, %s, %s)" % (res['vm_id'], res['cores_id'], res['os_id']))
+                    self.con.commit()
+                    agg_id = self.cur.lastrowid
+
+                    for term in self.term_list:
+                        output = self.agg_data(term, table, field, res['vm_id'])
+                        if output:
+                            self.cur.execute("UPDATE xiaoice_memoryaggdata SET %s_min = %s, %s_25 = %s, %s_75 = %s, %s_max = %s, %s_median = %s WHERE id = %s" % (term, output['resultmin'], term, output['result25'], term, output['result75'], term, output['resultmax'], term, output['resultmedian'], agg_id))
+                            self.con.commit()
+                except Exception as e:
+                    pass
 
             if self.localdiskdata == "y":
                 table = "xiaoice_localdiskdata"
-
                 fields = ['iops_read_seq', 'iops_write_seq', 'iops_read_rand', 'iops_write_rand']
-                for field in fields:
-                    for term in self.term_list:
-                        output = self.agg_data(term, table, field)
+
+                try:
+                    self.cur.execute("SELECT vm_id, cores_id, os_id FROM %s WHERE vm_id = %s" % (table, result['id']))
+                    res = self.cur.fetchone()
+
+                    self.cur.execute("INSERT INTO xiaoice_localdiskaggdata (vm_id, cores_id, os_id) VALUES (%s, %s, %s)" % (res['vm_id'], res['cores_id'], res['os_id']))
+                    self.con.commit()
+                    agg_id = self.cur.lastrowid
+
+                    for field in fields:
+                        for term in self.term_list:
+                            output = self.agg_data(term, table, field, res['vm_id'])
+                            if output:
+                                self.cur.execute("UPDATE xiaoice_localdiskaggdata SET %s_%s_min = %s, %s_%s_25 = %s, %s_%s_75 = %s, %s_%s_max = %s, %s_%s_median = %s WHERE id = %s" % (field, term, output['resultmin'], field, term, output['result25'], field, term, output['result75'], field, term, output['resultmax'], field, term, output['resultmedian'], agg_id))
+                                self.con.commit()
+                except Exception as e:
+                    pass
 
             if self.internalnetworkdata == "y":
                 table = "xiaoice_internalnetworkdata"
-
                 fields = ['single_threaded_throughput', 'multi_threaded_throughput']
-                for field in fields:
-                    for term in self.term_list:
-                        output = self.agg_data(term, table, field)
 
-    def agg_data(self, term, table, field):
+                try:
+                    self.cur.execute("SELECT vm_id, cores_id, os_id FROM %s WHERE vm_id = %s" % (table, result['id']))
+                    res = self.cur.fetchone()
 
-        # Fetches aggregate data for the previous month
-        if term is 'prev_month':
-            self.cur.execute("SELECT %s FROM %s WHERE YEAR(timestamp) = YEAR(CURDATE() - INTERVAL 1 MONTH) AND MONTH(timestamp) = MONTH(CURDATE() - INTERVAL 1 MONTH)" % (field, table))
-        # Fetches aggregate data from one year back to current date
-        elif term is 'prev_year':
-            self.cur.execute("SELECT %s FROM %s WHERE timestamp BETWEEN DATE_SUB(CURDATE(), INTERVAL 1 YEAR) AND CURDATE()" % (field, table))
-        # Fetches aggregate data from the beginning to current date
-        elif term is 'lifetime':
-            self.cur.execute("SELECT %s FROM %s" % (field, table))
+                    self.cur.execute("INSERT INTO xiaoice_internalnetworkaggdata (vm_id, cores_id, os_id) VALUES (%s, %s, %s)" % (res['vm_id'], res['cores_id'], res['os_id']))
+                    self.con.commit()
+                    agg_id = self.cur.lastrowid
+
+                    for field in fields:
+                        for term in self.term_list:
+                            output = self.agg_data(term, table, field, res['vm_id'])
+                            if output:
+                                self.cur.execute("UPDATE xiaoice_internalnetworkaggdata SET %s_%s_min = %s, %s_%s_25 = %s, %s_%s_75 = %s, %s_%s_max = %s, %s_%s_median = %s WHERE id = %s" % (field, term, output['resultmin'], field, term, output['result25'], field, term, output['result75'], field, term, output['resultmax'], field, term, output['resultmedian'], agg_id))
+                                self.con.commit()
+                except Exception as e:
+                    pass
+
+    def agg_data(self, term, table, field, vm_id):
+
+        try:
+            # Fetches aggregate data for the previous month
+            if term is 'month':
+                self.cur.execute("SELECT %s FROM %s WHERE YEAR(timestamp) = YEAR(CURDATE() - INTERVAL 1 MONTH) AND MONTH(timestamp) = MONTH(CURDATE() - INTERVAL 1 MONTH) AND vm_id = %s" % (field, table, vm_id))
+            # Fetches aggregate data from one year back to current date
+            elif term is 'year':
+                self.cur.execute("SELECT %s FROM %s WHERE timestamp BETWEEN DATE_SUB(CURDATE(), INTERVAL 1 YEAR) AND CURDATE() AND vm_id = %s" % (field, table, vm_id))
+            # Fetches aggregate data from the beginning to current date
+            elif term is 'lifetime':
+                self.cur.execute("SELECT %s FROM %s WHERE vm_id = %s" % (field, table, vm_id))
+        except Exception as e:
+            pass
 
         results = self.cur.fetchall()
-        results = [element for tupl in results for element in tupl]
-        results = filter(None, results)
+        results = [d[field] for d in results if field in d]
 
-        output = []
+        output = {}
         if results:
             output['resultmin'] = min(results)
             output['result25'] = percentile(results, 25)
