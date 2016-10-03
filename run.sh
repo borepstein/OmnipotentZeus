d963cffeb4bf72bfbb039051a1928de22d7d0361
Sibi M <sibim024@gmail.com>
2016-10-03 15:38:22 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 # apt-get update
-# apt-get install python-pip mysql-server libmysqlclient-dev python-dev python-lxml --yes
-# pip install -r requirements.txt
+apt-get install python-pip mysql-server libmysqlclient-dev python-dev python-lxml --yes
+pip install -r requirements.txt
 # screen python base.py
 python base.py
