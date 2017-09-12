fa5597d2d43537f8fe56b711558b21c302231b9a
borepstein <borepstein@gmail.com>
2017-09-12 17:49:23 -0400

    Working version.

--- ycsb_test_run.py
+++ ycsb_test_run.py
@@ -19,11 +19,11 @@ class YCSBOutputFile():
 
             if len(fields) != 3: continue            
 
-            if fields[0] == "[OVERALL]":
-                if fields[1] == "RunTime(ms)":
-                    self.__dataTable['overall_run_time_ms'] = fields[2]
-                if fields[1] == "Throughput(ops/sec)":
-                    self.__dataTable['overall_thruput_ops'] = fields[2]
+            if fields[0] not in self.__dataTable.keys():
+                self.__dataTable[ fields[0] ] = {}
+                
+            self.__dataTable[fields[0]][fields[1]] = fields[2]
+            
         fhandle.close()
         
     # end __init__(self, outputFile)
@@ -60,34 +60,88 @@ class YCSBTestSpecFile():
 class YCSBIndTest():
     # begin  __init__(self, testFile, loadResFile, runResFile):
 
-    def __init__(self, testFile, loadResFile, runResFile):
-        __testWorkload = None
-        __loadRes = None
-        __runRes = None
-        self.__testWorkload = YCSBTestSpecFile(testFile)
-        self.__loadRes = YCSBOutputFile(loadResFile)
-        self.__runRes = YCSBOutputFile(runResFile)
+    def __init__(self, testFile, loadResFile, runResFile, summaryOutputFile = None,
+                 provider = None, clusterSize=None, threadCount=None, dbDeviceType=None,
+                 dbReplFactor=None):
+        self.__dataTable = {}
+        self.__dataTable['ycsb_test_file_desc'] = YCSBTestSpecFile(testFile)
+        self.__dataTable['ycsb_load_out_file_desc'] = YCSBOutputFile(loadResFile)
+        self.__dataTable['ycsb_run_out_file_desc'] = YCSBOutputFile(runResFile)
+        self.__dataTable['output_file_path'] = summaryOutputFile
+        self.__dataTable['provider'] = provider
+        self.__dataTable['cluster_size'] = clusterSize
+        self.__dataTable['thread_count'] = threadCount
+        self.__dataTable['db_device_type'] = dbDeviceType
+        self.__dataTable['db_repl_factor'] = dbReplFactor
     # end  __init__(self, testFile, loadResFile, runResFile):
 
-    # begin
+    # begin printSummary(self)
     def printSummary(self):
-        testWLData = self.__testWorkload.getDataTable()
-        loadData = self.__loadRes.getDataTable()
-        runData = self.__runRes.getDataTable()
-        
-        print testWLData['recordcount'] + ',' + testWLData['operationcount'] + ',' + \
-            testWLData['fieldcount'] + ',' + testWLData['fieldlength'] + ',' + \
-            loadData['overall_run_time_ms'] + ',' \
-            + loadData['overall_thruput_ops'] + ',' + \
-            runData['overall_run_time_ms'] + ',' + \
-            runData['overall_thruput_ops']
+        testWLData = self.__dataTable['ycsb_test_file_desc'].getDataTable()
+        loadData = self.__dataTable['ycsb_load_out_file_desc'].getDataTable()
+        runData = self.__dataTable['ycsb_run_out_file_desc'].getDataTable()
+
+        outputStr = self.__dataTable['provider'] + ',' 
+        outputStr += self.__dataTable['cluster_size'] + ','
+        outputStr += self.__dataTable['thread_count'] + ','
+        outputStr += self.__dataTable['db_device_type'] + ','
+        outputStr += self.__dataTable['db_repl_factor'] + ','
+        outputStr += testWLData['recordcount'] + ',' + testWLData['operationcount'] + ','
+        outputStr += testWLData['fieldcount'] + ',' + testWLData['fieldlength'] + ','
+        outputStr += loadData['[OVERALL]']['RunTime(ms)'] + ',' 
+        outputStr += loadData['[OVERALL]']['Throughput(ops/sec)'] + ','
+        outputStr += loadData['[INSERT]']['AverageLatency(us)'] + ','
+        outputStr += loadData['[INSERT]']['MinLatency(us)'] + ','
+        outputStr += loadData['[INSERT]']['MaxLatency(us)'] + ','
+        outputStr += loadData['[INSERT]']['95thPercentileLatency(us)'] + ','
+        outputStr += loadData['[INSERT]']['99thPercentileLatency(us)'] + ','
+        outputStr += runData['[OVERALL]']['RunTime(ms)'] + ',' 
+        outputStr += runData['[OVERALL]']['Throughput(ops/sec)']
+
+        if '[READ]' in runData.keys():
+            outputStr += runData['[READ]']['AverageLatency(us)'] + ','
+            outputStr += runData['[READ]']['MinLatency(us)'] + ','
+            outputStr += runData['[READ]']['MaxLatency(us)'] + ','
+            outputStr += runData['[READ]']['95thPercentileLatency(us)'] + ','
+            outputStr += runData['[READ]']['99thPercentileLatency(us)'] + ','
+        else:
+            outputStr += "Null" + ','
+            outputStr += "Null" + ','
+            outputStr += "Null" + ','
+            outputStr += "Null" + ','
+            outputStr += "Null" + ','
+
+        if '[UPDATE]' in runData.keys():
+            outputStr += runData['[UPDATE]']['AverageLatency(us)'] + ','
+            outputStr += runData['[UPDATE]']['MinLatency(us)'] + ','
+            outputStr += runData['[UPDATE]']['MaxLatency(us)'] + ','
+            outputStr += runData['[UPDATE]']['95thPercentileLatency(us)'] + ','
+            outputStr += runData['[UPDATE]']['99thPercentileLatency(us)']
+        else:
+            outputStr += "Null" + ','
+            outputStr += "Null" + ','
+            outputStr += "Null" + ','
+            outputStr += "Null" + ','
+            outputStr += "Null" 
+
+        if self.__dataTable['output_file_path'] is None:
+            print outputStr
+            return
+
+        fh = open( self.__dataTable['output_file_path'], 'w')
+        fh.write( outputStr )
+        fh.close()
             
-    # end
+    # end printSummary(self)
+
+    def getDataTable(self): return self.__dataTable()
         
 # end YCSBIndTest
 
 
 # main block
 if __name__ == "__main__":
-    testHandler = YCSBIndTest(sys.argv[1], sys.argv[2], sys.argv[3])
+    testHandler = YCSBIndTest(sys.argv[1], sys.argv[2], sys.argv[3],
+                              sys.argv[4], sys.argv[5], sys.argv[6],
+                              sys.argv[7], sys.argv[8], sys.argv[9],)
     testHandler.printSummary()
